436db2e235dd9e5031d180190597e83ff58aea3d
ARAVINDAKUMAR S <119474792+renfieldaravind35@users.noreply.github.com>
2022-11-30 14:49:14 +0530

    Add files via upload

---
+++ src/main/letsgo/LetsGoApplicationTests.java
@@ -0,0 +1,41 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "source": [
+        "package se.firmacarboo.letsgo;\n",
+        "\n",
+        "import org.junit.jupiter.api.Test;\n",
+        "import org.springframework.boot.test.context.SpringBootTest;\n",
+        "\n",
+        "@SpringBootTest\n",
+        "class LetsGoApplicationTests {\n",
+        "\n",
+        "    @Test\n",
+        "    void contextLoads() {\n",
+        "    }\n",
+        "\n",
+        "}"
+      ],
+      "metadata": {
+        "id": "bso-as9RHLU0"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}

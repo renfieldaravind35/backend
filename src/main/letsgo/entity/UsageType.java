6eab021dffe7b82fc55b94586e3a2aaabe14d210
ARAVINDAKUMAR S <119474792+renfieldaravind35@users.noreply.github.com>
2022-11-30 14:43:26 +0530

    Add files via upload

---
+++ src/main/letsgo/entity/UsageType.java
@@ -0,0 +1,33 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "source": [
+        "package se.firmacarboo.letsgo.entity;\n",
+        "\n",
+        "public enum UsageType {\n",
+        "    BRUSHHAIR, BRUSHTEETH, EATBREAKFAST, GETDRESSED, GOTOSCHOOL, PUTONOUTERWEAR, USELOTION\n",
+        "}"
+      ],
+      "metadata": {
+        "id": "IhnQVTrfNkyy"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
